1e16c921a9407576c91ced2370ad689ee8fb70f0
William Dembinski <wdembiz@gmail.com>
2015-02-08 13:39:37 -0800

    Fixed some evidence that would suggest I got lazy and started copy-pasting snippets :)..it never happened

--- js/controllers.js
+++ js/controllers.js
@@ -2,16 +2,16 @@
 
 var blackHabbit = angular.module('blackHabbit', []);
 
-blackHabbit.controller('homeCtrl', ['$scope', 'Phone',
-  function($scope, Phone) {
-    $scope.phones = Phone.query();
-    $scope.orderProp = 'age';
+blackHabbit.controller('homeCtrl', ['$scope', 'result',
+  function($scope, result) {
+    $scope.results = result.query();
+    $scope.orderProp = 'relevance';
   }]);
 
-blackHabbit.controller('ResultDetailCtrl', ['$scope', '$routeParams', 'Phone',
-  function($scope, $routeParams, Phone) {
-    $scope.phone = Phone.get({phoneId: $routeParams.phoneId}, function(phone) {
-      $scope.mainImageUrl = phone.images[0];
+blackHabbit.controller('ResultDetailCtrl', ['$scope', '$routeParams', 'result',
+  function($scope, $routeParams, result) {
+    $scope.result = result.get({resultId: $routeParams.resultId}, function(result) {
+      $scope.mainImageUrl = result.images[0];
     });
 
     $scope.setImage = function(imageUrl) {
