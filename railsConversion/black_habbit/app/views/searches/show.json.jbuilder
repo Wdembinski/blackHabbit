dfbf2e514d661c33b9812e646086559b026ddf7f
William Dembinski <wdembiz@gmail.com>
2015-02-11 23:25:41 -0800

    Added a search view, need to work on refactoring the css and handling some cross browser stuff.  Still need to do some rails

---
+++ railsConversion/black_habbit/app/views/searches/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @search, :id, :userQuery, :created_at, :updated_at
--- railsConversion/black_habbit/db/schema.rb
+++ railsConversion/black_habbit/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150211174924) do
+ActiveRecord::Schema.define(version: 20150212063017) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(version: 20150211174924) do
     t.integer  "expires_in"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "searches", force: :cascade do |t|
+    t.string   "userQuery"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
